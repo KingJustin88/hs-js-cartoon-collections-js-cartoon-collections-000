108936c8249b99fc25d78ba3d04238045f7e96fd
Justin Hirata <jlhirata88@aol.com>
2019-01-31 21:09:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ function longPlaneteerCalls(words) {
 }
 function findTheCheese(foods) {
   for(let i = 0; i < foods.length; i += 1){
-    } if(foods[i] == 'cheddar' || foods[i] == 'gouda' || foods[i] == 'camembert' ){
+  if(foods[i] == 'cheddar' || foods[i] == 'gouda' || foods[i] == 'camembert' ){
     return 'cheddar'
     }
+  }
     return `no cheese!`
 }
 
